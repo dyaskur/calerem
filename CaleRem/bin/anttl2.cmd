19afad801c67443b6034391f1ff2b30fbc287c99
pli1885@gmail.com <pli1885@gmail.com@e0817914-cf66-8ef2-6da0-c6d823da1cd8>
2013-03-07 14:32:59 +0000

    new staff -bin/CaleRem.apk (logika to .apk pou 8elei) -bin/MainActivity-debug-unaligned.apk -bin/MainActivity-debug-unaligned.apk.d -bin/MainActivity-debug.apk -bin/MainActivity-release-unsigned.apk -bin/MainActivity.ap_ -bin/anttl2.cmd (script sto cmd gia automati ektelesi tou ANT) -bin/build.prop -bin/classes.dex -bin/classes/com/example/calerem/BuildConfig.class -bin/resources.ap_
    Ston fakelo jar/ briskete to jar arxeio tis efarmogis

---
+++ CaleRem/bin/anttl2.cmd
@@ -0,0 +1,8 @@
+cd C:\Users\Public\Documents\calerem\CaleRem
+ant release
+ant debug
+ant jar
+cd C:\Program Files\Java\jdk1.7.0_15\sdk\tools
+emulator -avd emulator -avd AVD_for_Nexus_One_by_Google
+
+PAUSE

a566991c9ba5493cb20ec8517d2bc527fc340da7
agapspyr@gmail.com <agapspyr@gmail.com@e0817914-cf66-8ef2-6da0-c6d823da1cd8>
2013-03-10 11:12:25 +0000

    c_Facebook_API

---
+++ CaleRem/src/com/example/calerem/c_Facebook_API.java
@@ -0,0 +1,14 @@
+package com.example.calerem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class c_Facebook_API extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+	}
+	//public c_event f_parse_file(){
+	
+	//}
+}

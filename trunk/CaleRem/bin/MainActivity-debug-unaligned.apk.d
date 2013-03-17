24ea5db77c0a7236d2e5dc4f539a092c98770729
agapspyr@gmail.com <agapspyr@gmail.com@e0817914-cf66-8ef2-6da0-c6d823da1cd8>
2013-03-17 13:53:21 +0000

    Done some changes on c_facebook_API

--- trunk/CaleRem/bin/MainActivity-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Public\Documents\Trunk\CaleRem\bin\MainActivity-debug-unaligned.apk : \
-C:\Users\Public\Documents\Trunk\CaleRem\bin\MainActivity.ap_ \
-C:\Users\Public\Documents\Trunk\CaleRem\bin\classes.dex \
-C:\Users\Public\Documents\Trunk\CaleRem\src\com\example\calerem\run_me.bat \
-C:\Users\Public\Documents\Trunk\CaleRem\libs\android-support-v4.jar \
--- trunk/CaleRem/bin/MainActivity-release-unsigned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\Public\Documents\calerem\CaleRem\bin\MainActivity-release-unsigned.apk : \
-C:\Users\Public\Documents\calerem\CaleRem\bin\MainActivity.ap_ \
-C:\Users\Public\Documents\calerem\CaleRem\bin\classes.dex \
-C:\Users\Public\Documents\calerem\CaleRem\libs\android-support-v4.jar \

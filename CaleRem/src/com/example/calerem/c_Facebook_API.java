25bbf64826a569d3c17a7b1e85c42d60895e90bd
agapspyr@gmail.com <agapspyr@gmail.com@e0817914-cf66-8ef2-6da0-c6d823da1cd8>
2013-03-25 15:03:26 +0000

    Progress done on c_Facebook_API

--- CaleRem/src/com/example/calerem/c_Facebook_API.java
+++ CaleRem/src/com/example/calerem/c_Facebook_API.java
@@ -1,22 +1,30 @@
 package com.example.calerem;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.facebook.LoggingBehavior;
+import com.facebook.Request;
+import com.facebook.Request.GraphUserListCallback;
+import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.Settings;
+import com.facebook.model.GraphUser;
 
 
 
-public class c_facebook_API extends Activity {
+public class c_Facebook_API extends Activity {
 	
 	private Session.StatusCallback statusCallback = new SessionStatusCallback();
 	private Session session;
 	private Bundle saved_Instance_State;
 	
-	public c_facebook_API(String username, String password)
+	public c_Facebook_API(String username, String password)
 	{	
 		loginActivity();
 	}
@@ -27,8 +35,8 @@ public class c_facebook_API extends Activity {
 		saved_Instance_State = savedInstanceState;
 	}
 	
-	public void loginActivity () {
-		Intent intent = new Intent(c_facebook_API.this, c_facebook_API.class);
+	private void loginActivity () {
+		Intent intent = new Intent(c_Facebook_API.this, c_Facebook_API.class);
         startActivity(intent);
 		
 		Settings.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
@@ -46,6 +54,44 @@ public class c_facebook_API extends Activity {
 				session.openForRead(new Session.OpenRequest(this).setCallback(statusCallback));
 			}
 		}	
+		getUserData(session);
+	}
+	
+	private void getUserData(final Session session) {
+		Request request = Request.newMeRequest(session, new Request.GraphUserCallback() {
+						
+			@Override
+			public void onCompleted(GraphUser user, Response response) {
+				if (user!= null && session == Session.getActiveSession()){
+					getFriends();
+				}
+				if(response.getError() != null) {
+					
+				}
+			}
+		});
+		request.executeAsync();
+	}
+	
+	private void getFriends(){
+
+		//String fqlQuery = "select uid, name, pic_square, is_app_user from user where uid in (select uid2 from friend where uid1 = me())";
+		//Bundle params = new Bundle();
+		//params.putString("q", fqlQuery);
+		Session activeSession = Session.getActiveSession();
+		if(activeSession.getState().isOpened()) {
+			Request friendRequest = Request.newMyFriendsRequest(activeSession,new GraphUserListCallback() {
+				
+				@Override
+				public void onCompleted(List<GraphUser> users, Response response) {
+					Log.i("INFO",response.toString());
+				}
+			});
+			Bundle params = new Bundle();
+			params.putString("fields", "id,name,friends");
+			friendRequest.setParameters(params);
+			friendRequest.executeAsync();
+		}
 	}
 	
 	private class SessionStatusCallback implements Session.StatusCallback {
@@ -55,3 +101,4 @@ public class c_facebook_API extends Activity {
 		}
 	}
 }
+

3a82eecef332654ef6e3bda9803481b6f1a317f2
pli1885@gmail.com <pli1885@gmail.com@e0817914-cf66-8ef2-6da0-c6d823da1cd8>
2013-05-13 22:31:26 +0000

    Παράδοση 14/5/2013

--- trunk/Calerem/bin/AndroidManifest.xml.d
+++ trunk/Calerem/bin/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-C:\Users\AnastasiaSwthria\Documents\AATEXNOLOGIA LOGISMIKOY\CaleRem Project\trunk\Calerem\bin\AndroidManifest.xml : \
-C:\Users\AnastasiaSwthria\Documents\AATEXNOLOGIA LOGISMIKOY\CaleRem Project\trunk\Calerem\AndroidManifest.xml \
+C:\Users\AnastasiaSwthria\Desktop\Kwstas\trunk\Calerem\bin\AndroidManifest.xml : \
+C:\Users\AnastasiaSwthria\Desktop\Kwstas\trunk\Calerem\AndroidManifest.xml \
